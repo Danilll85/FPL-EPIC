ca032653aef5c8b5694b81437dc74ce64139bcf8
Danilll85 <danilakuroedov85@gmail.com>
2025-03-16 21:28:46 +0300

    add task 6

---
+++ HW1/Task6.js
@@ -0,0 +1,19 @@
+const quadraticEquation = (a, b, c) => {
+    const D = b ** 2 - 4 * a * c;
+
+    if (D < 0) return [];
+
+    if (D == 0) {
+        return [(-b + Math.sqrt(D)) / 2];
+    }
+
+    if (D > 0) {
+        return [(-b + Math.sqrt(D)) / 2, (-b - Math.sqrt(D)) / 2];
+    }
+}
+
+console.log(quadraticEquation(1, -8 , 72));
+console.log(quadraticEquation(1, 12, 36));
+console.log(quadraticEquation(1, 6, 1));
+
+
