a0dfb6b9f0d1842544bb82cb5bb290a69ab515fc
Danilll85 <danilakuroedov85@gmail.com>
2025-05-18 21:34:23 +0300

    last update

--- HW6/src/components/TasksTableComponent/index.ts
+++ HW6/src/components/TasksTableComponent/index.ts
@@ -20,13 +20,16 @@ export const createTasksTableComponent = (tasksArr: task[] = currentTasks) => {
     const sortBtnsWrapper = document.createElement("div");
     sortBtnsWrapper.classList.add("sort-btns-wrapper");
 
-    const sortDescBtn = document.createElement("button");
-    sortDescBtn.textContent = "▲";
-
     const sortAscBtn = document.createElement("button");
-    sortAscBtn.textContent = "▼";
-    sortBtnsWrapper.appendChild(sortDescBtn);
+    sortAscBtn.textContent = "▲";
+    sortAscBtn.addEventListener("click", () => ascendingSort(tasksArr, i));
+
+    const sortDescBtn = document.createElement("button");
+    sortDescBtn.textContent = "▼";
+    sortAscBtn.addEventListener("click", () => descendingSort(tasksArr, i));
+
     sortBtnsWrapper.appendChild(sortAscBtn);
+    sortBtnsWrapper.appendChild(sortDescBtn);
 
     thContentWrapper.append(headerText, sortBtnsWrapper);
 
@@ -90,4 +93,57 @@ export const changeTaskStatus = (e: Event, index: number) => {
   localStorage.setItem(key, JSON.stringify(currentTasks));
   removeTasksTableComponent();
   createTasksTableComponent();
+};
+
+export const ascendingSort = (tasks: task[], index: number) => {
+  const sortedTasks = [...tasks];
+  switch (index) {
+    case 0: 
+      sortedTasks.sort((a, b) => (a.isCompleted === b.isCompleted ? 0 : a.isCompleted ? 1 : -1));
+      break;
+    case 1: 
+      sortedTasks.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case 2: 
+      const priorityOrder = { high: 1, medium: 2, low: 3 };
+      sortedTasks.sort(
+        (a, b) =>
+          priorityOrder[a.priority as keyof typeof priorityOrder] -
+          priorityOrder[b.priority as keyof typeof priorityOrder]
+      );
+      break;
+
+    case 3: 
+      sortedTasks.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+      break;
+  }
+
+  removeTasksTableComponent();
+  createTasksTableComponent(sortedTasks);
+};
+
+export const descendingSort = (tasks: task[], index: number) => {
+  const sortedTasks = [...tasks];
+  switch (index) {
+    case 0: // Done
+      sortedTasks.sort((a, b) => (a.isCompleted === b.isCompleted ? 0 : a.isCompleted ? -1 : 1));
+      break;
+    case 1: // Title
+      sortedTasks.sort((a, b) => b.title.localeCompare(a.title));
+      break;
+    case 2: // Priority
+      const priorityOrder = { high: 1, medium: 2, low: 3 };
+      sortedTasks.sort(
+        (a, b) =>
+          priorityOrder[a.priority as keyof typeof priorityOrder] -
+          priorityOrder[b.priority as keyof typeof priorityOrder]
+      );
+      break;
+    case 3: // Date
+      sortedTasks.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      break;
+  }
+
+  removeTasksTableComponent();
+  createTasksTableComponent(sortedTasks);
 };
