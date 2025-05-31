f49b571f785d6fb1fc3425644088e60410bea906
Danilll85 <danilakuroedov85@gmail.com>
2025-05-31 20:14:24 +0300

    update fourth task

--- HW9/src/utils/mergeObjects.ts
+++ HW9/src/utils/mergeObjects.ts
@@ -1,3 +1,19 @@
 export const mergeObjects = <T extends object, U extends object>(a: T, b: U): T & U => {
-  return { ...a, ...b };
+  return Object.entries(b).reduce(
+    (acc, [key, value]) => {
+      const typedKey = key as keyof (T & U); 
+
+      if (typeof value === "object" && value !== null && key in acc) {
+        if (typeof acc[typedKey] === "object" && acc[typedKey] !== null) {
+          acc[typedKey] = mergeObjects(acc[typedKey] as object, value as object) as any;
+        } else {
+          acc[typedKey] = value as any;
+        }
+      } else {
+        acc[typedKey] = value;
+      }
+      return acc;
+    },
+    { ...a } as T & U
+  );
 };
