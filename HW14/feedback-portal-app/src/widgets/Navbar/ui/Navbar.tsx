b53dc7db05668fa3f820ebcb9e49acfc24ef80c4
Danilll85 <danilakuroedov85@gmail.com>
2025-06-28 18:14:33 +0300

    fix auth

--- HW14/feedback-portal-app/src/widgets/Navbar/ui/Navbar.tsx
+++ HW14/feedback-portal-app/src/widgets/Navbar/ui/Navbar.tsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AppTitle, AuthItem, NavbarWrapper, Navigation, NavItem } from "./styles";
 import { LoginForm } from "../../../features/auth/ui/LoginForm/ui";
 import { ModalOverlay, ModalContent, CloseButton } from "./styles";
 import close from "../../../assets/close_icon.svg";
+import { store, type AppDispatch, type RootState } from "../../../app/providers/store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuth } from "../../../app/providers/store/slices/auth.slice";
 
 export const Navbar = () => {
   const [isShowAuthForm, setIsShowAuthForm] = useState(false);
-  const [isLogged, setIsLogged] = useState(false);
+  const isLogged = useSelector((state: RootState) => state.auth.isAuth);
+  const dispatch = useDispatch<AppDispatch>();
+
+
+  const handleClick = () => {
+    if (!isLogged) {
+      setIsShowAuthForm(true);
+      return;
+    }
+
+    dispatch(setAuth(false));
+  };
 
   return (
     <NavbarWrapper>
@@ -14,16 +28,16 @@ export const Navbar = () => {
       <Navigation>
         <NavItem>Home</NavItem>
         <NavItem>About</NavItem>
-        <AuthItem onClick={() => setIsShowAuthForm(true)}>{isLogged ? "logOut" : "Login"}</AuthItem>
+        <AuthItem onClick={handleClick}>{isLogged ? "logOut" : "Login"}</AuthItem>
       </Navigation>
 
-      {isShowAuthForm && (
+      {isShowAuthForm && !isLogged && (
         <ModalOverlay onClick={() => setIsShowAuthForm(false)}>
           <ModalContent onClick={(e) => e.stopPropagation()}>
             <CloseButton onClick={() => setIsShowAuthForm(false)}>
               <img src={close} alt="Close modal" />
             </CloseButton>
-            <LoginForm showModal={setIsShowAuthForm}/>
+            <LoginForm showModal={setIsShowAuthForm} />
           </ModalContent>
         </ModalOverlay>
       )}
